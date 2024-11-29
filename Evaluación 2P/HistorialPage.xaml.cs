0cf3b208d68391798bb7f09c0ffaf6bc7b930468
VanessaTorres004 <vanessa.torres.chacon@udla.edu.ec>
2024-11-29 08:35:35 -0500

    historial page

--- Evaluación 2P/HistorialPage.xaml.cs
+++ Evaluación 2P/HistorialPage.xaml.cs
@@ -7,3 +7,37 @@ public partial class HistorialPage : ContentPage
 		InitializeComponent();
 	}
 }
+private void LoadHistorialRecargas()
+    {
+        var recargas = new List<Recarga>();
+
+        // Aquí puedes cargar los datos desde archivos o desde una base de datos
+        // Este es solo un ejemplo de cómo podrías hacerlo
+        string[] files = Directory.GetFiles(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "*.txt");
+
+        foreach (var file in files)
+        {
+            var contenido = File.ReadAllText(file);
+            var datos = contenido.Split(',');
+
+            // Asegúrate de que el archivo tenga al menos 2 elementos
+            if (datos.Length >= 2)
+            {
+                recargas.Add(new Recarga
+                {
+                    PhoneNumber = datos[0],
+                    MontoRecarga = datos[1]
+                });
+            }
+        }
+
+        RecargasListView.ItemsSource = recargas;
+    }
+}
+
+public class Recarga
+{
+    public string PhoneNumber { get; set; }
+    public string MontoRecarga { get; set; }
+}
+}
