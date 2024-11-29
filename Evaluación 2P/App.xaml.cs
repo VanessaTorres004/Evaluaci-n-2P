2d2e76ab96b71f2269a53f5a4bfa54a1e63386f0
VanessaTorres004 <vanessa.torres.chacon@udla.edu.ec>
2024-11-29 07:06:47 -0500

    Agregar archivos de proyecto.

---
+++ Evaluación 2P/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Evaluación_2P
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
